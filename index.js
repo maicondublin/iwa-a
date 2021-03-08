6dfcb9f684d818dbd0d2786cc6f5cdc496a44ce4
maicondublin <maicondublin@gmail.com>
2021-03-08 23:03:06 +0000

    wk3 code API

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const http = require('http');
-const axios = require('axios');
+const http = require('http'),
+axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'), //our router 
+bodyParser = require('body-parser');
 
-http.createServer((req, res)=>{
-//res.write("Hello BSc3! \n");//write a response
-// res.write(users.join(", ")); //display the list of users on the page
-// res.write("\n\n"+emails.join(", ")); 
-res.write(chars.join(", "));
-res.end();//end the response
-}).listen(8000);//listen for response on port 8000
+var app = express();
+var port = 8000;
+
+app.use(bodyParser.json()) //json and parser 
+
+app.get('/hello/:foo/:bar', (req, res) =>{ //to play around 'trick'
+   res.json({message: 'Hello BScBest!', data: [
+       req.params.foo,
+       req.params.bar
+   ]});
+   });
+// //use the below one 
+// app.get('/hello', (req, res) =>{
+//    res.json({message: 'Hello BScBest!'}); //json file ??!
+// // res.write("Hello BScBest!")
+// //res.send('Hello BScBest!' + '\n\n' + chars.join('\n'));
+// // res.send('Hello BScBest!');
+// // res.write(users.join('\n'));
+// });//first router or root
+
+// http.createServer((req, res)=>{
+// //res.write("Hello BSc3! \n");//write a response
+// // res.write(users.join(", ")); //display the list of users on the page
+// // res.write("\n\n"+emails.join(", ")); 
+// res.write(chars.join(", "));
+// res.end();//end the response
+// }).listen(8000);//listen for response on port 8000
 
 // let users = []; //name of users will be stored here.
 // axios.get("https://jsonplaceholder.typicode.com/users")
@@ -37,3 +61,7 @@ let chars = [];
         console.log(error)
     }
 })()
+
+app.listen(port, function(err){
+    console.log('Listining on port:' + port)
+});
