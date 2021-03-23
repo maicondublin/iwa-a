f57a38bd22c81251a47fa5cbcada705b7b095619
maicondublin <maicondublin@gmail.com>
2021-03-23 10:22:40 +0000

    week6 mongodb

--- routes.js
+++ routes.js
@@ -1,18 +1,26 @@
 const express = require('express'), //using express for routing
 router = express.Router();
 
+var itemCtrl = require('./item-controller');
+
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/bar', itemCtrl.getWorldParams);
+router.post('/hello', itemCtrl.postWorld);
+
+
 //routing line 17 to 24 
 //end point line 14 
 //changed app to router below.
-router.get('/hello/:foo/:bar', (req, res) =>{ //to play around 'trick'
-   res.json({message: 'Hello BScBest!', data: [
-       req.params.foo,
-       req.params.bar
-   ]});
-   });
-//post requesting 
-   router.post('/hello', (req, res) => {
-       res.json({result: 'Post was sent', data: req.body});
-   });
-//"tricky" 
+// router.get('/hello/:foo/:bar', (req, res) =>{ //to play around 'trick'
+//    res.json({message: 'Hello BScBest!', data: [
+//        req.params.foo,
+//        req.params.bar
+//    ]});
+//    });
+// //post requesting 
+//    router.post('/hello', (req, res) => {
+//        res.json({result: 'Post was sent', data: req.body});
+//    });
+// //"tricky" 
 module.exports = router;
