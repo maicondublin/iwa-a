237f295bbdc4a53d85e59e2776505cd7d9fe4fae
maicondublin <maicondublin@gmail.com>
2021-04-09 10:42:19 +0000

    week-8 update

--- models/image.js
+++ models/image.js
@@ -1,13 +1,13 @@
-var nongoose = require('nongoose');
+var mongoose = require('mongoose');
 
-var imageSchema = new nongoose.imageSchema({
+var imageSchema = new mongoose.Schema({
     filename: String,
     originalName: String,
     desc: String,
-    // created: Date dont need thid because of the timestamps
+    
 },
     { timestamps: true }
 );
 
-module.exports = nongoose.model('Image', imageSchema);
+module.exports = mongoose.model('Image', imageSchema);
 
